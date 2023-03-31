989a64df50f34c5f28d1225478c49faf2079b206
AarthiSaminathan <127826312+AarthiSaminathan@users.noreply.github.com>
2023-03-31 12:21:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+using NLog;
+using NLog.Web;
+using NLogDemo;
+using System;
+
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+   
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
+
+    //other classes that need the logger 
+    builder.Services.AddTransient<GenericHelper>();
+
+    var app = builder.Build();
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    
+
+
+
+
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch(Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
+
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
